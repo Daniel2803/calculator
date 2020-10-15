ee9779042521b1672c0cca89655181260472e8cd
Daniel2803 <49502893+Daniel2803@users.noreply.github.com>
2020-10-15 15:33:02 +1300

    Add files via upload

---
+++ Calculator Challenge/calculator.js
@@ -0,0 +1,31 @@
+
+function calcNumbers(){
+
+
+var op = document.getElementByClass("numbers").value
+
+if(op === "+"){
+
+document.getElementByClass('answer').value = n1+n2;
+
+}
+if(op === "-"){
+
+document.getElementByClass('answer').value = n1-n2;
+
+}
+if(op === "*"){
+
+document.getElementByClass('answer').value = n1*n2;
+
+}
+if(op === "/"){
+
+document.getElementByClass('answer').value = n1/n2;
+
+}
+
+
+
+
+}
